a8f14f39e35172fae91c388b12f562dcdabbef46
AkramQureshi <161283057+AkramQureshi@users.noreply.github.com>
2024-03-31 22:46:43 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,38 @@
+
+let user ="ratan lal"
+let otp ="123"
+
+if(user == "ratan lal"){
+console.log("Sending Code ........")
+}
+else console.log("Invalid user")
+
+if(otp !="123"){
+    
+    console.log(" ")
+    console.log("Searching ............")
+    console.log(" ")
+    console.log("Welcome", user)}
+
+else
+{
+    console.log(" ")
+    console.log("Searching ............")
+    console.log(" ")
+    console.log("invalid otp")}
+
+
+
+/*
+let yourName="akram"
+let empNo="203356"
+
+if(yourName=="akram")
+    console.log("Your Name is :", yourName)
+    if(empNo!="203356")
+console.log("Your Employee # is:", empNo)
+else{
+    console.log("Invalid User... Try Again....")
+}
+*/
+
